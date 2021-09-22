cf78ecef7b8525577a81cfaedec871f90c53897f
Vinit Gupta <vinitman35@gmail.com>
2021-09-22 18:29:23 +0530

    first commit:add header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import  Header  from "./components/Header.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top : 10px;
+  width: 100vw;
+  height: 90vh;
+}
+.container {
+  width : 90%;
+  height: 60%;
+  margin: 0 auto;
+  padding: 20px;
+  border: 2px solid #323d23;
+  border-radius: 5px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        Task Tracker
+    </header>
+</template>
+
+<script>
+export default {
+    name : "Header"
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom : 20px;
+        font-size: 2rem;
+        font-weight: 600;
+    }
+</style>
